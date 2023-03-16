e7719d081f4fbea6c1014b83fd91a62414e6a91e
Anaelle Mimifir <anaellemimi27@gmail.com>
2023-03-16 08:10:21 +0100

    update

--- src/ihm/PanelFormulaire.java
+++ src/ihm/PanelFormulaire.java
@@ -12,6 +12,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.List;
+import javax.swing.JDialog;
 
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
